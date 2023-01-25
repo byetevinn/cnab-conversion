56eb53c5f09b4351e6b37155555c5866d0f0e3dc
byetevinn <stevan14padilha@gmail.com>
2023-01-25 12:13:06 -0300

    Fix: Choices Transaction Type

--- transactions/models.py
+++ transactions/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 
+TRANSACTION_TYPE = {
+    1: "Débito",
+    2: "Boleto",
+    3: "Financiamento",
+    4: "Crédito",
+    5: "Recebimento Empréstimo",
+    6: "Vendas",
+    7: "Recebimento TED",
+    8: "Recebimento DOC",
+    9: "Aluguel",
+}
+
+
 class Transactions(models.Model):
-    type = models.IntegerField(max_length=1)
+    type = models.IntegerField(choices=TRANSACTION_TYPE.items())
     date = models.IntegerField(max_length=8)
     value = models.IntegerField(max_length=10)
     cpf = models.IntegerField(max_length=11)
