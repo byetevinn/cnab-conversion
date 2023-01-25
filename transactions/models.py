4ec09acce38634cd8b43ebd9bc3f5bb80885d2c5
byetevinn <stevan14padilha@gmail.com>
2023-01-25 11:20:11 -0300

    Feat: Model Transaction

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Transactions(models.Model):
+    type = models.IntegerField(max_length=1)
+    date = models.IntegerField(max_length=8)
+    value = models.IntegerField(max_length=10)
+    cpf = models.IntegerField(max_length=11)
+    card = models.CharField(max_length=12)
+    hour = models.IntegerField(max_length=6)
+    store_owner = models.CharField(max_length=14)
+    store_name = models.CharField(max_length=19)
