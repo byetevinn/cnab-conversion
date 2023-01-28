bb23744feaa0536ce852b3caf3b7b7baa5ce0efe
byetevinn <stevan14padilha@gmail.com>
2023-01-28 20:40:32 -0300

    Fix: STATIC_ROOT

--- _core/settings.py
+++ _core/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = BASE_DIR / "staticfiles"
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
