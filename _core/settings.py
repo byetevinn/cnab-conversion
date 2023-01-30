f095bf6f4ceb2af83a40ab1935ecbe59a4f4dfea
byetevinn <stevan14padilha@gmail.com>
2023-01-30 00:50:44 -0300

    Feat: CORS_ALLOW_METHODS

--- _core/settings.py
+++ _core/settings.py
@@ -180,3 +180,12 @@ CORS_ALLOW_HEADERS = [
     "x-csrftoken",
     "x-requested-with",
 ]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
